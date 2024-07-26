cb4b2841e0d69d097eb29f9025e76b83d2e93fc8
poteto <naonyan1135@gmail.com>
2024-07-26 23:58:00 +0900

    morgan作れるじゃん

--- t.js
+++ t.js
@@ -2,13 +2,13 @@ const express = require("express")
 const app = express()
 const morgan = require("morgan")
 
-app.use((req,res,next)=>{
-  req.accessTime = Date.now()
+app.use((req, res, next) => {
+  console.log(req.method, req.path) //* GET /
   next()
 })
 
-app.get("/",(req,res)=>{
-  res.send(`<h1>${req.accessTime}</h1>`)
+app.get("/", (req, res) => {
+  res.send(`<h1>hello</h1>`)
 })
 
 app.listen(3001, () => {
