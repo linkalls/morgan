d93a59b288d685a9d923b33f086c977eaf098f5d
poteto <naonyan1135@gmail.com>
2024-07-26 23:35:00 +0900

    add

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const express = require("express")
 const app = express()
+const morgan = require("morgan")
+
+
+app.use(morgan("tiny"))//* すべてのリクエストでapp.use内のコールバック関数は実行される
+//* GET /dogs 304 - - 2.507 ms
+//* GET /w2d2cf3vg 404 148 - 2.180 ms
 
 app.get("/",(req,res)=>{
 res.send("<h1>homepage</h1>")
